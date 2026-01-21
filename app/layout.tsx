63055e007d6462f1a08979fa272a0c29c4d2f8e9
jooho2075 <parkjh2075@icloud.com>
2026-01-21 15:03:05 +0900

    260121_app/layout.tsx 오류 제거(주석 제거)

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body> {/*antialiased : 글꼴을 부드럽게 만들어주는 클래스*/}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
